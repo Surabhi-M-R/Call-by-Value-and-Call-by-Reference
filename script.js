a1c5bd03cc7618cdf3229e342a540c70623ec76b
Surabhi M R <surabhimr27@gmail.com>
2025-04-11 18:38:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-// pass by value
-// let a=10;
-// const modifyValue=(x)=>(x=20);
-// console.log(modifyValue(a));
-// console.log(a);
+pass by value
+let a=10;
+const modifyValue=(x)=>(x=20);
+console.log(modifyValue(a));
+console.log(a);
 
-// let obj={
-//     id:5,
-//     name:"hacker",
-// };
-// let obj1=obj;
-// console.log(obj1);
-// obj1.name="coder";
-// console.log(obj1);
-// console.log(" original::",obj);
+let obj={
+    id:5,
+    name:"hacker",
+};
+let obj1=obj;
+console.log(obj1);
+obj1.name="coder";
+console.log(obj1);
+console.log(" original::",obj);
 
 //obect.assign() is used to copy properties from one or more source objects to a target object.
 
-// let obj={ id:5,name:"hacker"};
-// let newObj=Object.assign({},obj);
-// newObj.name="coder";
-// console.log(newObj);
-// console.log(obj);
+let obj={ id:5,name:"hacker"};
+let newObj=Object.assign({},obj);
+newObj.name="coder";
+console.log(newObj);
+console.log(obj);
 
 // comparision by reference
-// const obj1={ sName:" sush"};
-// const obj2={ sName:" sush"};
-// const isEqual = obj1==obj2 ? true: false;
-// console.log(isEqual);
+const obj1={ sName:" sush"};
+const obj2={ sName:" sush"};
+const isEqual = obj1==obj2 ? true: false;
+console.log(isEqual);
 
 //converted to JSON and viceversa
 
 // let student={
-//     id:1,
-//     sName:29,
-//     isStudent:false,
-//     greet:function(){
-//         console.log(`hey my id is ${student.id} and my name is ${student.sName}`);
-//     },
-// };
-// console.log(student);
-// let jsonData=JSON.stringify(student);
-// console.log(jsonData);
-// let parseData=JSON.parse(jsonData);
-// console.log(parseData);
+    id:1,
+    sName:29,
+    isStudent:false,
+    greet:function(){
+        console.log(`hey my id is ${student.id} and my name is ${student.sName}`);
+    },
+};
+console.log(student);
+let jsonData=JSON.stringify(student);
+console.log(jsonData);
+let parseData=JSON.parse(jsonData);
+console.log(parseData);
 
 // this key word
 //  in an object this refers to global object
@@ -54,45 +54,45 @@
 
 
 //* regular function Expression
-// const obj={
-//     sName: "  coder",
-//     greet:function(){
-//         console.log(this);
-//     },
-// };
-// obj.greet();
+const obj={
+    sName: "  coder",
+    greet:function(){
+        console.log(this);
+    },
+};
+obj.greet();
 
-// const product={
-//     id:1,
-//     pName:"laptop",
-//     category:" computers",
-//     brand:" example brand",
-//     price:999.99,
-//     stock:50,
-//     image:" image",
-//};
+const product={
+    id:1,
+    pName:"laptop",
+    category:" computers",
+    brand:" example brand",
+    price:999.99,
+    stock:50,
+    image:" image",
+};
 // // to obtain keys
-// let key=Object.keys(product);
-// console.log(key);
+let key=Object.keys(product);
+console.log(key);
 // to obtain values
-// let value=Object.values(product);
-// console.log(value);
+let value=Object.values(product);
+console.log(value);
 // to obtain values in the form of array
-// let entries=Object.entries(product);
-// console.log(entries);
+let entries=Object.entries(product);
+console.log(entries);
 // object.hasOwnProperty() : returns a boolean indicating whether the object has the specified property as an own property
-// console.log(product.hasOwnProperty("pName"));
-// console.log(product.hasOwnProperty("cost"));
+console.log(product.hasOwnProperty("pName"));
+console.log(product.hasOwnProperty("cost"));
 // Object.assign(): Copies the values of all enumerable own properties from one or more source objects to a targeted object and also helps in pass by reference
-// const target={a:1,b:2};
-// const source={b:3,d:4};
-// const mergedObjects=Object.assign(target,source);
-// console.log(mergedObjects);
+const target={a:1,b:2};
+const source={b:3,d:4};
+const mergedObjects=Object.assign(target,source);
+console.log(mergedObjects);
 
 // Object.freeze(): Freezes an object,preventing new properties from being added to it and existing 
-// Object.freeze(product);
-// product.id="556";
-// console.log(product);
+Object.freeze(product);
+product.id="556";
+console.log(product);
 //===========================================================================================================================================
 // interView question  1) given an array object representing  a student,write a function to add a new subject with its corresponding grade to the student's record. also check if the grades property is present or not
 //============================================================================================================================================
